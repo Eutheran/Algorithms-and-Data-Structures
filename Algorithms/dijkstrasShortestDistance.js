0cd3c5f4f5baef4b0ae5c014ed0e1bd77514c935
Eric K <kreitere@gmail.com>
2019-09-18 15:53:46 -0400

    update: finished dijkstras shortest path algo with full priority queue

--- Algorithms/dijkstrasShortestDistance.js
+++ Algorithms/dijkstrasShortestDistance.js
@@ -64,7 +64,7 @@ class PriorityQueue {
         break;
       }
     }
-    return this.values;
+    return min;
   }
 }
 
@@ -91,6 +91,81 @@ class WeightedListGraph {
     }
     return this.adjacencyList;
   }
+
+  dijkstraShortestPath(start, finish) {
+    const previous = {};
+    const distances = {};
+    let smallest;
+    const shortestPath = [];
+    const priorityQueue = new PriorityQueue();
+
+    //set up initial state
+    for (let vertex in this.adjacencyList) {
+      if (start === vertex) {
+        distances[vertex] = 0;
+        priorityQueue.enqueue(vertex, 0);
+      } else {
+        distances[vertex] = Infinity;
+        priorityQueue.enqueue(vertex, Infinity);
+      }
+      previous[vertex] = null;
+    }
+    //while queue is not empty
+    while (priorityQueue.values.length > 0) {
+      smallest = priorityQueue.dequeue().value;
+      if (smallest === finish) {
+        //while a node exists on the path, eventually our START variable will set this value to NULL;
+        while (smallest) {
+          //we push each node along the way into our results array
+          shortestPath.push(smallest);
+          //we set smallest to the previous node until we hit START
+          smallest = previous[smallest];
+        }
+        //get out of our original loop, we're done
+        break;
+      }
+      //if smallest exists or isnt infinity
+      if (smallest || distances[smallest] !== Infinity) {
+        this.adjacencyList[smallest].forEach(adjacentNodeObj => {
+          //get nodes name
+          let adjacentNode = adjacentNodeObj.node;
+          //get nodes distance
+          let nodeDistance = adjacentNodeObj.weight;
+          //add the previous distance to our new nodes distance
+          let candidateRoute = distances[smallest] + nodeDistance;
+          //if our route to this node is shorter
+          if (candidateRoute < distances[adjacentNode]) {
+            //update the distance to the shorter route
+            distances[adjacentNode] = candidateRoute;
+            //update previous - how we got here from shortest path
+            previous[adjacentNode] = smallest;
+            //enqueue the adjacentNode and its total distance
+            priorityQueue.enqueue(adjacentNode, nodeDistance);
+          }
+        });
+      }
+    }
+    return shortestPath.reverse();
+  }
 }
 
 let graph = new WeightedListGraph();
+
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+
+graph.addEdge('A', 'B', 4);
+graph.addEdge('A', 'C', 2);
+graph.addEdge('B', 'E', 3);
+graph.addEdge('C', 'D', 2);
+graph.addEdge('C', 'F', 4);
+graph.addEdge('D', 'E', 3);
+graph.addEdge('D', 'F', 1);
+graph.addEdge('F', 'E', 1);
+
+graph.dijkstraShortestPath('A', 'E');
+//should return ['A', 'C', 'D', 'F', 'E']
